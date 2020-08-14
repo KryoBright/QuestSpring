6292a968a6a4f999e1cc791523574646a4162242
Kryo Bright <simon_second@mail.ru>
2020-08-14 11:41:40 +0800

    Table template added

---
+++ demo/src/main/java/com/example/quest/Controllers/TableController.java
@@ -0,0 +1,48 @@
+package com.example.quest;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.*;
+
+import com.example.quest.Response;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TableController {
+
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/room/table")
+	public Response table_desc() {
+		var response_text="This is the table.Probably the most ordinary one.There is typewriter on top of it,box of fortune cookies and 3 sections underneath";
+
+		return new Response(counter.incrementAndGet(), response_text);
+	}
+
+	@PutMapping("/room/table")
+	public Response table_note(@RequestBody Map<String, Object> payload) {
+		var response_text="You try to atttach note to the tables edge.";
+        if (payload.keySet().contains("content"))
+		    response_text="You write a note saying: '"+payload.get("content").toString()+"' and attach it.";
+        else
+            response_text="You decide not to write anything.Note instantly disappiers";
+        return new Response(counter.incrementAndGet(), response_text);
+	}
+
+	@PostMapping("/room/table")
+	public Response table_object() {
+		var response_text="You want to use some object on table.Unfortunately, you have none";
+		return new Response(counter.incrementAndGet(), response_text);
+	}
+
+	@DeleteMapping("/room/table")
+	public Response table_hit() {
+		var response_text="You hit table.It makes sound as if something was breaking inside it,but nothing changes.";
+		return new Response(counter.incrementAndGet(), response_text);
+	}
+}
